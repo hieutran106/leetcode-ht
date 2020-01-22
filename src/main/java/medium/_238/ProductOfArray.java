bc87fe19c7b5d56c0fa8e7aeabb15dcfd44b0d97
Hieu Tran <hieu.t.tran4@boeing.com>
2020-01-22 16:08:55 +1000

    feat: optimize solution for 238

--- src/main/java/medium/_238/ProductOfArray.java
+++ src/main/java/medium/_238/ProductOfArray.java
@@ -1,27 +1,29 @@
 package medium._238;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class ProductOfArray {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
 
-        int[] head = new int[nums.length];
+        // int[] head = new int[nums.length];
         int[] tail = new int[nums.length];
 
-        head[0] = 1;
+        // head[0] = 1;
         tail[0] = 1;
 
         for (int i =1; i < nums.length; i ++) {
-            head[i] = nums[i-1] * head[i -1];
+            //head[i] = nums[i-1] * head[i -1];
             int j = nums.length - i;
             tail[i] = nums[j] * tail[i-1];
+
         }
 
+        int sum = 1;
         for (int i =0; i < nums.length; i ++) {
-            int j =  (nums.length-1) -i;
-            result[i] = head[i] * tail[j];
+            int j =  (nums.length-1) - i;
+
+            result[i] = sum * tail[j];
+            sum = sum * nums[i];
         }
 
         return result;
