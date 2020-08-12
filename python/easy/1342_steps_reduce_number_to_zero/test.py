26a05b8e0b2c1beaca573d307822b776227b2bc6
Hieu Tran <hieu.t.tran4@boeing.com>
2020-08-12 11:51:54 +1000

    feat: solve 1342 - Step reduce a number to zero

--- python/easy/1342_steps_reduce_number_to_zero/test.py
+++ python/easy/1342_steps_reduce_number_to_zero/test.py
@@ -1,9 +1,32 @@
 import unittest
 from .solution import Solution
+from .solution2 import Solution2
 
 class TestSum(unittest.TestCase):
+    def getSolution():
+        return Solution2()
+
     def test_case_1(self):
-        self.assertEqual(3, 2)
+        s = TestSum.getSolution()
+        actual = s.numberOfSteps(14)
+        print(actual)
+        self.assertEqual(actual, 6)
+
+    def test_case_2(self):
+        s = TestSum.getSolution()
+        actual = s.numberOfSteps(8)
+        self.assertEqual(actual, 4)
+
+    def test_case_3(self):
+        s = TestSum.getSolution()
+        actual = s.numberOfSteps(123)
+        self.assertEqual(actual, 12)
+
+    def test_case_4(self):
+        s = TestSum.getSolution()
+        actual = s.numberOfSteps(0)
+        self.assertEqual(actual, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
