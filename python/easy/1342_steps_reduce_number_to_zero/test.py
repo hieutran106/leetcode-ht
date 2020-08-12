048cab66fec1d402179e7ef03ee7b600aa2ef84e
Hieu Tran <hieu.t.tran4@boeing.com>
2020-08-12 11:25:30 +1000

    refactor: add python folder

---
+++ python/easy/1342_steps_reduce_number_to_zero/test.py
@@ -0,0 +1,9 @@
+import unittest
+from .solution import Solution
+
+class TestSum(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(3, 2)
+
+if __name__ == '__main__':
+    unittest.main()
