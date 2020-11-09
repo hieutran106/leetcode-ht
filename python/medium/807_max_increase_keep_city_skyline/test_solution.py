70b5c3a2733fa3771f5e287e594446521e5e61a2
Hieu Tran <hieu.t.tran4@boeing.com>
2020-11-09 14:48:41 +1000

    feat: solve 921 - minimum add to make parentheses valid

--- python/medium/807_max_increase_keep_city_skyline/test_solution.py
+++ python/medium/807_max_increase_keep_city_skyline/test_solution.py
@@ -5,10 +5,10 @@ from .solution import Solution
 class TestSolution(unittest.TestCase):
     def test_case_1(self):
         s = Solution()
-        actual = s.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"])
-        self.assertEqual(actual, "ball")
+        grid = [[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]
+        actual = s.maxIncreaseKeepingSkyline(grid)
+        self.assertEqual(actual, 35)
 
 
 if __name__ == '__main__':
     unittest.main()
-
