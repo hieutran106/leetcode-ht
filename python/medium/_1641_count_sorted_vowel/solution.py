a5a142b709f6201c05e713bf82cd47752313146c
Hieu Tran <hieu.t.tran4@boeing.com>
2020-11-17 08:28:35 +1000

    feat: solve 1475 - final prices discount

--- python/medium/_1641_count_sorted_vowel/solution.py
+++ python/medium/_1641_count_sorted_vowel/solution.py
@@ -6,10 +6,11 @@ class Solution:
             row_sum = 0
             row = [0] * 5
             for j in reversed(range(5)):
-                row[j] = result[-1][j] + row_sum
+                row[j] = result[-1][j] + (row[j+1] if j <4 else 0)
                 row_sum += result[-1][j]
 
             result.append(row)
 
         final = sum(result[-1])
         return final
+
