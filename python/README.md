39bf26e25de5875fd4d36f4cd82322813dc20e84
Hieu Tran <hieu.t.tran4@boeing.com>
2020-11-23 09:58:26 +1000

    feat: generate README.md

---
+++ python/README.md
@@ -0,0 +1,3 @@
+
+My leetcode solution collection
+===============================
---
+++ python/create_readme_md.py
@@ -0,0 +1,5 @@
+from mdutils.mdutils import MdUtils
+
+
+mdFile = MdUtils(file_name='README.md',title='My leetcode solution collection')
+mdFile.create_md_file()
