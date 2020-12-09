1641221f0b40e9498cfac2e33023719afef56d03
Hieu Tran <hieu.t.tran4@boeing.com>
2020-12-09 11:21:12 +1000

    feat: solve 0/1 knapsack problem

--- python/others/_01_knapsack_problem/solution.py
+++ python/others/_01_knapsack_problem/solution.py
@@ -1,20 +1,45 @@
-import math
+from typing import List
 
 class Solution:
-    def solve(self, weights, values, max_weight):
-        dp = [0 for i in range(max_weight + 1)] * (len(values) + 1)
-        for i in range(len(values) + 1):
-            for j in range(len(max_weight) + 1):
-                without_current = dp[i - 1][j]
+    def solve(self, values: List[int], weights: List[int], capacity: int):
+        rows = len(values) + 1
+        cols = capacity + 1
+        dp = [[0] * cols for _ in range(rows)]
+        for i in range(1, rows):
+            for c in range(cols):
+                profit1 = dp[i - 1][c]
 
-                with_current = 0
-                weight = weights[i]
-                value = values[i]
-                if weight < j:
-                    with_current = value + dp[i][j - weight]
+                profit2 = 0
+                curr_weight = weights[i - 1]
+                if curr_weight <= c:
+                    profit2 = values[i - 1] + dp[i-1][c - curr_weight]
 
                 # compare
-                dp[i][j] = max(without_current, with_current)
+                dp[i][c] = max(profit1, profit2)
+
+        return dp[rows-1][cols-1]
 
 
-        return dp[len(values)][max_weight]
+class Solution3:
+    '''
+    Bruce force solution
+    '''
+
+    def solve(self, values: List[int], weights: List[int], capacity: int) -> int:
+        # start from item 0
+        return self.knapsack_recursive(values, weights, capacity, 0)
+
+    def knapsack_recursive(self, values, weights, capacity, index) -> int:
+        # base check
+        if capacity < 0 or index >= len(values):
+            return 0
+
+        # include item at index
+        profit1 = 0
+        if weights[index] <= capacity:
+            profit1 = values[index] + self.knapsack_recursive(values, weights, capacity - weights[index], index + 1)
+
+        # not include item at index
+        profit2 = self.knapsack_recursive(values, weights, capacity, index + 1)
+
+        return max(profit1, profit2)
--- python/others/_01_knapsack_problem/test_solution.py
+++ python/others/_01_knapsack_problem/test_solution.py
@@ -1,21 +1,28 @@
 import unittest
-from .solution import Solution
+from .solution import Solution, Solution3
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.s = Solution()
 
     def test_case1(self):
-        actual = self.s.solve([4, 5, 1], [1, 2, 3], 4)
-        self.assertEqual(actual, 3)
+        values = [4, 5, 3, 7 ]
+        weights = [2, 3, 1, 4 ]
+        actual = self.s.solve(values, weights, capacity=5)
+        self.assertEqual(actual, 10)
 
     def test_case2(self):
-        actual = self.s.solve([4, 5, 6], [1, 2, 3], 3)
-        self.assertEqual(actual, 0)
+        values = [1, 6, 10, 16]
+        weights = [1, 2, 3, 5]
+        actual = self.s.solve(values, weights, capacity=7)
+        self.assertEqual(actual, 22)
 
     def test_case3(self):
-        actual = self.s.solve([10, 20 , 30], [60, 100, 120], 50)
-        self.assertEqual(actual, 220)
+        values = [1, 6, 10, 16]
+        weights = [1, 2, 3, 5]
+        actual = self.s.solve(values, weights, capacity=6)
+        self.assertEqual(actual, 17)
+
 
 if __name__ == '__main__':
     unittest.main()
