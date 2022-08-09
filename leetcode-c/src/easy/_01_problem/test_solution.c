6ba2da4ff99e6783c86f3b9975639b53fa6a6ebf
hieutran106 <hieutran106@gmail.com>
2022-08-09 13:24:45 +1000

    Consolidate C setup

--- leetcode-c/src/easy/_01_problem/test_solution.c
+++ leetcode-c/src/easy/_01_problem/test_solution.c
@@ -19,17 +19,18 @@ void test_hello_world2()
     assert_float_equal(100, result, 0.01);
 }
 
-void test_fixture_hello( void )
-{
- 
-    run_test(test_hello_world);   
-    run_test(test_hello_world2);
- 
+void test_case_1() {
+    assert_int_equal(4, 4);
+}
+
+void test_case_2() {
+    assert_int_equal(5, 5);
 }
 
 void all_tests(void)
 {
-    test_fixture_hello();
+    run_test(test_case_1);
+    run_test(test_case_2);
 }
 
 int main(int argc, char **argv)
