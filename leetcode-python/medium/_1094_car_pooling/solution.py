a1194b6731fd27ce91025e6b7d35f1c69a1355e0
hieutran106 <hieutran106@gmail.com>
2022-09-26 15:43:32 +1000

    Solve 731 Medium - My calendar 2

--- leetcode-python/medium/_1094_car_pooling/solution.py
+++ leetcode-python/medium/_1094_car_pooling/solution.py
@@ -4,21 +4,17 @@ import heapq
 
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips.sort(key=lambda trip: trip[1], reverse=True)
-        print(trips)
-        min_heap = []  # (curr_to, c)
-        total_capacity = 0
-        curr_index = trips[-1][1]
-        while min_heap or trips:
-            while trips and trips[-1][1] == curr_index:
-                # Pull trip into heap
-                c, curr_from, curr_to = trips.pop()
-                total_capacity += c
-                heapq.heappush(min_heap, (curr_to, c))
-            while min_heap and min_heap[0][0] <= curr_index:
-                _, c = heapq.heappop(min_heap)
-                total_capacity -= c
-            if total_capacity > capacity:
+        trips.sort(key=lambda trip: (trip[1], trip[2]))
+        # print(trips)
+        min_heap = []  # store (end, c)
+        curr_capacity = 0
+        for c, start, end in trips:
+            curr_capacity += c
+            heapq.heappush(min_heap, (end, c))
+
+            while min_heap and min_heap[0][0] <= start:
+                _, capacity_out = heapq.heappop(min_heap)
+                curr_capacity -= capacity_out
+            if curr_capacity > capacity:
                 return False
-            curr_index += 1
         return True
--- leetcode-python/medium/_731_my_calendar_2/solution.py
+++ leetcode-python/medium/_731_my_calendar_2/solution.py
@@ -1,6 +1,7 @@
 import heapq
 from typing import List, Tuple
-import copy
+
+
 class MyCalendarTwo:
     bookings: List[Tuple[int, int]]
 
@@ -8,27 +9,20 @@ class MyCalendarTwo:
         self.bookings = []
 
     def book(self, start: int, end: int) -> bool:
-        bookings = copy.deepcopy(self.bookings)
-        bookings = list(filter(lambda b: b[1] >= start, bookings))
-        bookings.append((start, end))
-        bookings.sort(reverse=True)
-        print(bookings)
-
-        book_num = 0
-        min_heap = [] # save (b_end)
-
-        for i in range(start, end):
-            while bookings and i >= bookings[-1][0]:
-                b_start, b_end = bookings.pop()
-                book_num += 1
-                heapq.heappush(min_heap, b_end)
-
-            while min_heap and min_heap[0] == i:
-                book_num -= 1
-                heapq.heappop(min_heap)
-            if book_num > 2:
-                return False
-
-        # save booking
         self.bookings.append((start, end))
+
+        sorted_books = sorted(self.bookings)
+        booking_count = 0
+        min_heap = []  # save (b_end)
+
+        for current_start, current_end in sorted_books:
+            booking_count += 1
+            heapq.heappush(min_heap, current_end)
+            while min_heap and min_heap[0] <= current_start:
+                booking_count -= 1
+                heapq.heappop(min_heap)
+
+            if booking_count > 2:
+                self.bookings.pop()
+                return False
         return True
